5494a3f8f530110613dabf2dcd6b88bc26b623a2
Johannes Rieke <johannes.rieke@snowflake.com>
2024-12-24 22:36:10 +0100

    Remove test files

--- file_page2.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("File Page 2")
-st.write("This is page 2 content")
--- file_page3.py
+++
@@ -1,4 +0,0 @@
-import streamlit as st
-
-st.title("File Page 3")
-st.write("This is page 3 content")
